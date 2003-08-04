19486166f107bd1ff2da24cf7d8762062f57c7d8
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2003-08-04 07:40:47 +0000

    menu order and represent message fix

--- xoops_version.php
+++ xoops_version.php
@@ -35,10 +35,10 @@ $modversion['blocks'][1]['options'] = "0|10";
 
 // Menu
 $modversion['hasMain'] = 1;
-$modversion['sub'][1]['name'] = _MI_EGUIDE_REG;
-$modversion['sub'][1]['url'] = "reserv.php?op=register";
-$modversion['sub'][2]['name'] = _MI_EGUIDE_SUBMIT;
-$modversion['sub'][2]['url'] = "admin.php";
+$modversion['sub'][1]['name'] = _MI_EGUIDE_SUBMIT;
+$modversion['sub'][1]['url'] = "admin.php";
+$modversion['sub'][2]['name'] = _MI_EGUIDE_REG;
+$modversion['sub'][2]['url'] = "reserv.php?op=register";
 
 // Search
 $modversion['hasSearch'] = 1;
