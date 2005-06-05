3c0bdf286115379986c3e7de9b06c2d5fbbdfe2c
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2005-06-05 01:38:36 +0000

    backend.php support module chanel feeding

--- include/search.inc.php
+++ include/search.inc.php
@@ -1,30 +1,9 @@
 <?php
-// ------------------------------------------------------------------------- //
-//                XOOPS - PHP Content Management System                      //
-//                       <http://www.xoops.org/>                             //
-// ------------------------------------------------------------------------- //
-// Based on:								     //
-// myPHPNUKE Web Portal System - http://myphpnuke.com/	  		     //
-// PHP-NUKE Web Portal System - http://phpnuke.org/	  		     //
-// Thatware - http://thatware.org/					     //
-// ------------------------------------------------------------------------- //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-// ------------------------------------------------------------------------- //
-function eguide_search($queryarray, $andor, $limit, $offset, $userid){
+
+function eguide_search($queryarray, $andor, $limit, $offset, $userid, $desc){
 	global $xoopsDB;
-	$sql = "SELECT eid,uid,title,edate FROM ".$xoopsDB->prefix("eguide")." WHERE status=0";
+	$opt = $desc?", summary description":"";
+	$sql = "SELECT eid,uid,title,edate$opt FROM ".$xoopsDB->prefix("eguide")." WHERE status=0";
 	//$sql .= " AND expire>".time();
 	if ( $userid != 0 ) {
 		$sql .= " AND uid=".$userid." ";
@@ -39,7 +18,7 @@ function eguide_search($queryarray, $andor, $limit, $offset, $userid){
 		}
 		$sql .= ") ";
 	}
-	$sql .= "ORDER BY edate DESC";
+	$sql .= " ORDER BY edate DESC";
 	$result = $xoopsDB->query($sql,$limit,$offset);
 	$ret = array();
 	$i = 0;
@@ -49,6 +28,7 @@ function eguide_search($queryarray, $andor, $limit, $offset, $userid){
 		$ret[$i]['title'] = $myrow['title'];
 		$ret[$i]['time'] = $myrow['edate'];
 		$ret[$i]['uid'] = $myrow['uid'];
+		if ($desc) $ret[$i]['description'] = $myrow['description'];
 		$i++;
 	}
 	return $ret;
