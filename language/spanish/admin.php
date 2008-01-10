c130622e99265eedda885e55e945cbfcf7c6e06f
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2008-01-10 06:42:07 +0000

    add language/spanish (contrib from Gerardo)

---
+++ language/spanish/admin.php
@@ -0,0 +1,33 @@
+<?php
+// Event Guide Module Administration message catalogue
+// $Id: admin.php,v 1.1 2008/01/10 06:42:07 nobu Exp $
+define("_AM_DBUPDATED","Base de datos actualizada");
+define("_AM_TITLE","Título");
+define("_AM_POSTER","Autor");
+define("_AM_EDITARTICLE","Editar un Evento");
+define("_AM_TIME_FMT","m-d H:i");
+define("_AM_POST_FMT","Y-m-d H:i");
+define("_AM_POSTED","Publicado el");
+define("_AM_YES","Si");
+define("_AM_NO","No");
+define("_AM_OPERATION","Operación");
+define("_AM_EDIT","Status");
+define("_AM_DISP_STATUS","Status");
+define("_AM_ADD","Agregar");
+define("_AM_UPDATE","Actualizar");
+define("_AM_CATEGORY","Categorías");
+define("_AM_CATEGORY_NEW","Nueva Categoría");
+define("_AM_CAT_NAME","Nombre de la Categoría");
+define("_AM_CAT_IMG","Imagen");
+define("_AM_CAT_DESC","Descripción");
+
+define("_AM_SUMMARY", "Sumario de Reservación");
+
+define("_AM_CANCEL","Cancelar");
+define("_AM_EMAIL","Email");
+
+define("_AM_RESERVATION","Reservación");
+define("_AM_EVENT_DAY","Fecha del Evento");
+define("_AM_RECEIPT","Listado");
+define("_AM_COUNT","Contador");
+?>
---
+++ language/spanish/blocks.php
@@ -0,0 +1,12 @@
+<?php
+// Blocks
+define("_BLOCK_DATE_FMT", "m/d");
+define("_BLOCK_EV_POST", "Autor");
+define("_BLOCK_EV_WAIT", "Nuevos Eventos");
+define("_BLOCK_EV_STYLE","Mostrar todos");
+define("_BLOCK_EV_ITEMS","Mostrar Eventos en bloque");
+define("_BLOCK_EV_TRIM", "Límite de longitud del título");
+define("_BLOCK_EV_NONE", "Actualmente no hay Eventos");
+define("_BLOCK_EV_CATEGORY", "Categoría");
+define("_BLOCK_EV_MORE", "más...");
+?>
