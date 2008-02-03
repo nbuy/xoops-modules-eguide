f544789bb7b1cdf2c4255f39a6a55ce8faf280da
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2008-02-03 16:40:23 +0000

    add category filter option.

--- blocks/ev_cat.php
+++ blocks/ev_cat.php
@@ -1,6 +1,6 @@
 <?php
 # Event category select block 
-# $Id: ev_cat.php,v 1.1 2008/02/03 15:28:51 nobu Exp $
+# $Id: ev_cat.php,v 1.2 2008/02/03 16:40:23 nobu Exp $
 
 include dirname(dirname(__FILE__))."/mydirname.php";
 
@@ -16,13 +16,15 @@ if (function_exists("b_event_select_show_base")) return;
 function b_event_select_base($dirname, $prefix, $options) {
     global $xoopsDB, $xoopsUser;
     $cat = $xoopsDB->prefix($prefix.'_category');
-    $result = $xoopsDB->query("SELECT c.catid, c.catname AS name, c.catimg AS image,if (p.weight, p.weight, c.weight) ord1, if(p.weight IS NULL, -1, c.weight) ord2,c.catpri FROM $cat c LEFT JOIN $cat p ON c.catpri=p.catid ORDER BY ord1,ord2,catid");
+    $cats = $options[0];
+    $opt = preg_match('/^(\d+,)*\d+$/', $cats)?" WHERE p.catid IN ($cats)":"";
+    $result = $xoopsDB->query("SELECT c.catid, c.catname AS name, c.catimg AS image,if (p.weight, p.weight, c.weight) ord1, if(p.weight IS NULL, -1, c.weight) ord2,c.catpri FROM $cat c LEFT JOIN $cat p ON c.catpri=p.catid $opt ORDER BY ord1,ord2,catid");
     $list = array();
     $catid = isset($_GET['cat'])?intval($_GET['cat']):0;
     while ($data=$xoopsDB->fetchArray($result)) {
 	$id = $data['catid'];
 	$name = htmlspecialchars($data['name']);
-	if (!empty($data['catpri'])) $name = '-- '.$name;
+	if (!empty($data['catpri']) && empty($cats)) $name = '-- '.$name;
 	$list[] = array('name'=>$name, 'catid'=>$id, 'image'=>$data['image']);
     }
     return array('options'=>$list,
