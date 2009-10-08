b8ae4c5709657e3dab3eaaf959f538ae84997253
nbuy <nobuhiro.yasutomi@nifty.ne.jp>
2009-10-08 10:28:55 +0000

    add new control plugin daily limit

---
+++ plugins/language/english/dlimit.php
@@ -0,0 +1,2 @@
+<?php
+define('_PI_EGUIDE_DLIMIT_FULL_OF_DAY', 'Already reserved limit in a day: %d times');
---
+++ plugins/language/ja_utf8/dlimit.php
@@ -0,0 +1,2 @@
+<?php
+define('_PI_EGUIDE_DLIMIT_FULL_OF_DAY', '一日の予約件数が制限 (%d) に達しています');
